2b6b125a2315115a5d36d15d0eb9bd6cbb7d21ef
Александр Мехаев <alexandmekhaev@yandex.ru>
2024-11-05 23:12:54 +0300

    ya_note.notes.tests.test_logic

--- ya_note/notes/tests/conftest.py
+++ ya_note/notes/tests/conftest.py
@@ -9,12 +9,12 @@ from notes.models import Note
 
 @pytest.fixture
 # Используем встроенную фикстуру для модели пользователей django_user_model.
-def author(django_user_model):  
+def author(django_user_model):
     return django_user_model.objects.create(username='Автор')
 
 
 @pytest.fixture
-def not_author(django_user_model):  
+def not_author(django_user_model):
     return django_user_model.objects.create(username='Не автор')
 
 
@@ -50,3 +50,12 @@ def slug_for_args(note):
     # И возвращает кортеж, который содержит slug заметки.
     # На то, что это кортеж, указывает запятая в конце выражения.
     return (note.slug,)
+
+
+@pytest.fixture
+def form_data():
+    return {
+        'title': 'Новый заголовок',
+        'text': 'Новый текст',
+        'slug': 'new-slug'
+    }
