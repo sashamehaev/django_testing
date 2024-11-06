a22330fc6e58fa53768877034d38425b5efac727
Имя Фамилия <alexandrmekhaev@yandex.ru>
2024-11-06 22:56:41 +0300

    unittest notes.test_content

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -46,7 +46,6 @@ def test_pages_contains_form(author_client, name, args):
     assert 'form' in response.context
     # Проверяем, что объект формы относится к нужному классу.
     assert isinstance(response.context['form'], NoteForm)"""
-from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
@@ -73,16 +72,6 @@ class TestContent(TestCase):
         )
         cls.user = User.objects.create(username='Мимо Крокодил')
 
-        """all_news = [
-            News(
-                title=f'Новость {index}',
-                text='Просто текст.',
-                date=today - timedelta(days=index)
-            )
-            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-        ]
-        News.objects.bulk_create(all_news)"""
-
     def test_notes_list_for_different_users(self):
         users_statuses = (
             (self.author, True),
@@ -90,67 +79,21 @@ class TestContent(TestCase):
         )
         url = reverse('notes:list')
         for user, note_in_list in users_statuses:
-            self.client.force_login(user)
-            response = self.client.get(url)
-            object_list = response.context['object_list']
-            assert (self.note in object_list) is note_in_list
+            with self.subTest():
+                self.client.force_login(user)
+                response = self.client.get(url)
+                object_list = response.context['object_list']
+                assert (self.note in object_list) is note_in_list
 
-
-
-
-
-
-
-
-
-
-
-    def test_news_count(self):
-        response = self.client.get(self.HOME_URL)
-        object_list = response.context['object_list']
-        news_count = object_list.count()
-        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
-
-    def test_news_order(self):
-        response = self.client.get(self.HOME_URL)
-        object_list = response.context['object_list']
-        all_dates = [news.date for news in object_list]
-        sorted_dates = sorted(all_dates, reverse=True)
-        self.assertEqual(all_dates, sorted_dates)
-
-
-"""class TestDetailPage(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.news = News.objects.create(
-            title='Тестовая новость', text='Просто текст.'
+    def test_pages_contains_form(self):
+        pages_contains = (
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,))
         )
-        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
-        cls.author = User.objects.create(username='Комментатор')
-        now = timezone.now()
-        for index in range(10):
-            comment = Comment.objects.create(
-                news=cls.news, author=cls.author, text=f'Tекст {index}',
-            )
-            comment.created = now + timedelta(days=index)
-            comment.save()
-
-    def test_comments_order(self):
-        response = self.client.get(self.detail_url)
-        self.assertIn('news', response.context)
-        news = response.context['news']
-        all_comments = news.comment_set.all()
-        all_timestamps = [comment.created for comment in all_comments]
-        sorted_timestamps = sorted(all_timestamps)
-        self.assertEqual(all_timestamps, sorted_timestamps)
-
-    def test_anonymous_client_has_no_form(self):
-        response = self.client.get(self.detail_url)
-        self.assertNotIn('form', response.context)
-
-    def test_authorized_client_has_form(self):
-        self.client.force_login(self.author)
-        response = self.client.get(self.detail_url)
-        self.assertIn('form', response.context)
-        self.assertIsInstance(response.context['form'], CommentForm)"""
+        for name, args in pages_contains:
+            with self.subTest():
+                self.client.force_login(self.author)
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                assert 'form' in response.context
+                assert isinstance(response.context['form'], NoteForm)
