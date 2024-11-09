f8e7f84e7a3a884aba1d47c1aa3a65d75d6da8bc
Имя Фамилия <alexandrmekhaev@yandex.ru>
2024-11-09 03:41:07 +0300

    pytest news.test_content

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -65,7 +65,6 @@ def test_news_count(client, all_news):
     response = client.get(url)
     object_list = response.context['object_list']
     news_count = object_list.count()
-    # Проверяем, что на странице именно 10 новостей.
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
@@ -75,11 +74,8 @@ def test_news_order(client, all_news):
     url = reverse('news:home')
     response = client.get(url)
     object_list = response.context['object_list']
-    # Получаем даты новостей в том порядке, как они выведены на странице.
     all_dates = [news.date for news in object_list]
-    # Сортируем полученный список по убыванию.
     sorted_dates = sorted(all_dates, reverse=True)
-    # Проверяем, что исходный список был отсортирован правильно.
     assert all_dates == sorted_dates
 
 
@@ -87,20 +83,26 @@ def test_news_order(client, all_news):
 def test_comments_order(client, news, all_comments):
     url = reverse('news:detail', kwargs={'pk': news.id})
     response = client.get(url)
-    # Проверяем, что объект новости находится в словаре контекста
-    # под ожидаемым именем - названием модели.
     assert 'news' in response.context
-    # Получаем объект новости.
     news = response.context['news']
-    # Получаем все комментарии к новости.
     all_comments = news.comment_set.all()
-    # Собираем временные метки всех комментариев.
     all_timestamps = [comment.created for comment in all_comments]
-    # Сортируем временные метки, менять порядок сортировки не надо.
     sorted_timestamps = sorted(all_timestamps)
-    # Проверяем, что временные метки отсортированы правильно.
     assert all_timestamps == sorted_timestamps
 
+
+@pytest.mark.django_db
+def test_anonymous_client_has_no_form(client, news):
+    url = reverse('news:detail', kwargs={'pk': news.id})
+    response = client.get(url)
+    assert 'form' not in response.context
+
+
+def test_authorized_client_has_form(author_client, news):
+    url = reverse('news:detail', kwargs={'pk': news.id})
+    response = author_client.get(url)
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], CommentForm)
 
 
 """# news/tests/test_content.py
