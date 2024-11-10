8137a414cf9eff033723a2e35370d689e16fb249
Имя Фамилия <alexandrmekhaev@yandex.ru>
2024-11-10 03:43:01 +0300

    notes.test_content fixed

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
 
 from notes.forms import NoteForm
 from notes.models import Note
@@ -8,41 +8,50 @@ from notes.models import Note
 User = get_user_model()
 
 
-class TestContent(TestCase):
+class TestFixture(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Лев Толстой')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.user = User.objects.create(username='Мимо Крокодил')
+        cls.user_client = Client()
+        cls.user_client.force_login(cls.user)
         cls.note = Note.objects.create(
             title='Заголовок',
             text='Текст заметки',
             slug='note-slug',
             author=cls.author,
         )
-        cls.user = User.objects.create(username='Мимо Крокодил')
+        cls.notes_list_url = reverse('notes:list')
+        cls.notes_edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.notes_add_url = reverse('notes:add')
+        
+        return super().setUpTestData()
+
+
+class TestContent(TestFixture):
 
     def test_notes_list_for_different_users(self):
         users_statuses = (
-            (self.author, True),
-            (self.user, False),
+            (self.author_client, True),
+            (self.user_client, False),
         )
-        url = reverse('notes:list')
         for user, note_in_list in users_statuses:
-            with self.subTest():
-                self.client.force_login(user)
-                response = self.client.get(url)
-                object_list = response.context['object_list']
-                assert (self.note in object_list) is note_in_list
+            with self.subTest(user=user, note_in_list=note_in_list):
+                response = user.get(self.notes_list_url)
+                notes = response.context['object_list']
+                self.assertIs(self.note in notes, note_in_list)
 
     def test_pages_contains_form(self):
         pages_contains = (
-            ('notes:add', None),
-            ('notes:edit', (self.note.slug,))
+            self.notes_add_url,
+            self.notes_edit_url
         )
-        for name, args in pages_contains:
+        for url in pages_contains:
             with self.subTest():
                 self.client.force_login(self.author)
-                url = reverse(name, args=args)
                 response = self.client.get(url)
-                assert 'form' in response.context
-                assert isinstance(response.context['form'], NoteForm)
+                self.assertIn('form', response.context)
+                self.assertIsInstance(response.context['form'], NoteForm)
